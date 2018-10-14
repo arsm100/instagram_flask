27b7f5c67085bbc190a647ce1c2fccd02e8dbdbf
Josh Teng <joshteng@me.com>
2018-10-14 18:12:23 +0800

    use no autoflush on entire db session

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# https://stackoverflow.com/questions/38795414/flask-sqlalchemy-disable-autoflush-for-the-whole-session
+db = SQLAlchemy(app, session_options={"autoflush": False})
+# db = SQLAlchemy(app)
 Migrate(app, db)
 
 login_manager = LoginManager()
@@ -148,8 +150,7 @@ def validation_preparation(func):
         except AttributeError:
             obj.validation_errors = []
 
-        with db.session.no_autoflush:
-            func(obj, key, value)
+        func(obj, key, value)
 
     return wrapper
 
