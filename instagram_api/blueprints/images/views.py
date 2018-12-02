a7702164db07b0e4420cb9f756f02ceb36c66ee9
Joshua Teng <joshteng@me.com>
2018-12-02 17:24:07 +0800

    api endpoint - user can see their images if logged in

--- instagram_api/blueprints/images/views.py
+++ instagram_api/blueprints/images/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint, request
+from flask import jsonify, Blueprint, request, make_response
 from instagram import app
 from models.image import Image
 from models.user import User
@@ -7,16 +7,45 @@ images_api_blueprint = Blueprint('images_api',
                              __name__,
                              template_folder='templates')
 
-def url(image_name):
-    return app.config['S3_LOCATION'] + image_name
 
 @images_api_blueprint.route('/', methods=['GET'])
 def index():
     if request.args.get('userId'):
-        images = Image.query.with_entities(Image.image_name).add_columns(url(Image.image_name)).filter_by(user_id = int(request.args['userId'])).all()
+        images = Image.query.with_entities(Image.image_name).add_columns(Image.url(Image.image_name)).filter_by(user_id = int(request.args['userId'])).all()
     else:
         images = Image.query.with_entities(Image.image_name).add_columns(url(Image.image_name)).all()
 
     images = [image[1] for image in images]
 
     return jsonify(images)
+
+@images_api_blueprint.route('/me', methods=['GET'])
+def show():
+    auth_header = request.headers.get('Authorization')
+
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'No authorization header found'
+        }
+
+        return make_response(jsonify(responseObject)), 401
+
+    user_id = User.decode_auth_token(auth_token)
+
+    user = User.query.get(user_id)
+
+    if user:
+        images = Image.query.with_entities(Image.image_name).add_columns(Image.url(Image.image_name)).filter_by(user_id = user.id).all()
+        images = [image[1] for image in images]
+
+        return jsonify(images)
+    else:
+        responseObject = {
+            'status': 'failed',
+            'message': 'Authentication failed'
+        }
+
+        return make_response(jsonify(responseObject)), 401
--- models/user.py
+++ models/user.py
@@ -235,6 +235,6 @@ class User(db.Model, UserMixin):
             payload = jwt.decode(auth_token, app.config.get('SECRET_KEY'))
             return payload['sub']
         except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
+            return 0
         except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
+            return 0
